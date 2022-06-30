a94493960a0702ab6d5e61a495ccfbb3fad0a71e
Gladson <Gladsonk22@hotmail.com>
2022-06-29 21:31:25 -0300

    versao inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+const nome = "Piedro"
+const sobrenome = "Mota"
+const data_nacismento = "08/07/2006"
+let altura = 1.74
+let numero_sapato = 43
+let idade = 15
+let celular = "(18) 99700-5333"
+let cor_favorita = "preto"
+
+
